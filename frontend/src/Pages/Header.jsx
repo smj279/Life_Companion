12735d4b5adf42141b2c13b28372f370268a1902
Sumit Majumder <sumitmajumder270@gmail.com>
2024-06-05 02:08:47 +0600

    Homepage Done

--- frontend/src/Pages/Header.jsx
+++ frontend/src/Pages/Header.jsx
@@ -8,43 +8,13 @@ const Header = () => {
       <div className="logo">
         <img src={logo} alt="Shaadi" />
       </div>
+      <div className="nav-links">
+        <a href="/login">Login</a>
+        <a href="/help">Help</a>
+      </div>
       <div className="form-container">
-        <h1>Trusted Matrimony & Matchmaking Service</h1>
+        <h1>Trusted matrimony to find your better half</h1>
         <form>
-          <div className="form-group">
-            <label htmlFor="lookingFor">I'm looking for a</label>
-            <select id="lookingFor">
-              <option>Woman</option>
-              <option>Man</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="age">aged</label>
-            <input type="number" id="age" placeholder="22" /> to
-            <input type="number" id="ageTo" placeholder="27" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="religion">of religion</label>
-            <select id="religion">
-              <option>Select</option>
-              <option>Hindu</option>
-              <option>Muslim</option>
-              <option>Christian</option>
-              <option>Sikh</option>
-              <option>Other</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="location">and living in</label>
-            <select id="location">
-              <option>Select</option>
-              <option>India</option>
-              <option>USA</option>
-              <option>UK</option>
-              <option>Canada</option>
-              <option>Other</option>
-            </select>
-          </div>
           <button type="submit">Let's Begin</button>
         </form>
       </div>
