b2d30c445acebf20563f0d90ed2a5cdf4a868887
Sumit Majumder <sumitmajumder270@gmail.com>
2024-06-05 04:49:11 +0600

    About our matrimony

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Pages/Header';
+import ContentSection from './Pages/ContentSection';
+
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <ContentSection />
     </div>
   );
 }
