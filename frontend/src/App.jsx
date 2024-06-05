e7c3ea19100742a3fcb356f27d132fb71dfb9f13
Sumit Majumder <sumitmajumder270@gmail.com>
2024-06-05 07:18:23 +0600

    extra changes to homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './Pages/Header';
 import ContentSection from './Pages/ContentSection';
+import NewSection from './Pages/NewSection';
+import CarouselSection from './Pages/CarouselSection';
 
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <NewSection />
+      <CarouselSection />
       <ContentSection />
+      
     </div>
   );
 }
