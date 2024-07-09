5482f22f8459e93f66f46b4f3743bf8c12ce57c9
Sumit Majumder <sumitmajumder270@gmail.com>
2024-07-09 22:51:35 +0600

    mongodb atlas

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  userName: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
