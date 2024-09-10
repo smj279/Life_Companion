a2c1720174dbc5a69f297f42551da02f9725a3a4
marjanOSD <marjan.cse.20220104106@aust.edu>
2024-09-10 16:30:46 +0600

    Full information page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ import CarouselSection from './Pages/CarouselSection';
 import ProfileForm from './Pages/ProfileForm';
 import AdditionalInfoForm from './Pages/AdditionalInfoForm';
 import Education from './Pages/Education';
-import MoreInfo from './Pages/MoreInfo';  // Make sure this path matches the new MoreInfo page
-import MoreInfo from './Pages/More_Info'; // Corrected import
+
+import FullInformation from './Pages/FullInformation'; // Ensure the import is correct
 import SignUp from './Pages/SignUp';
 import Login from './Pages/Login';
 import DashboardPage from './Pages/DashboardPage';
@@ -20,6 +20,7 @@ import Story3 from './Pages/Story3';
 import AboutUs from './Pages/About'; 
 import HelpPage from './Pages/HelpPage';
 import Profile from './Pages/Profile';
+
 import MessagePage from './Pages/MessagePage'; // Import MessagePage component
 
 import { UserProvider } from './context/UserContext'; // Import UserProvider
@@ -27,12 +28,11 @@ import './App.css';
 
 function App() {
   return (
-
-
     <UserProvider>
       <Router>
         <div className="app">
           <Routes>
+            {/* Main page */}
             <Route 
               path="/" 
               element={
@@ -45,19 +45,24 @@ function App() {
               } 
             />
             
+            {/* Other routes */}
             <Route path="/profile-form" element={<ProfileForm />} />
             <Route path="/additional-info" element={<AdditionalInfoForm />} />
             <Route path="/education" element={<Education />} />
-            <Route path="/more-info" element={<MoreInfo />} /> {/* Corrected route */}
+
+            {/* Full Information Route */}
+            <Route path="/full-information" element={<FullInformation />} /> {/* Correct route */}
+
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/story1" element={<Story1 />} />
             <Route path="/story2" element={<Story2 />} />
             <Route path="/story3" element={<Story3 />} />
-            <Route path="/about-us" element={<AboutUs />} /> {/* Use AboutUs component */}
+            <Route path="/about-us" element={<AboutUs />} />
             <Route path="/help" element={<HelpPage />} />
             <Route path="/profile" element={<Profile />} />
+            
             <Route path="/messages/:userId" element={<MessagePage />} /> {/* Route for chat */}
           </Routes>
         </div>
